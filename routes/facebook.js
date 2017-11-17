b676651660efad443ee712c1765cb3dfaedf7950
Louis <louismc4@hotmail.com>
2017-11-17 17:43:55 +0000

    ahhh

--- routes/facebook.js
+++ routes/facebook.js
@@ -78,6 +78,8 @@ router.post('/facebookpost', function(request, response){
     var picture = '';
     var token = '';
     
+    console.log(request.body);
+    
     for(var key in request.body){
         outerKey = JSON.parse(key);
         break;
@@ -91,10 +93,10 @@ router.post('/facebookpost', function(request, response){
         response.send({['fb_id'] : fb_id, ['message'] : "Error : Bad Values!"});
     } else {
         fb_id = outerKey['fb_id'];
-        latitude = outerKey['latitude'];
-        longitude = outerKey['longitude'];
-        altitude = outerKey['altitude'];
-        status = outerKey['status'];
+        latitude = Number(outerKey['latitude']);
+        longitude = Number(outerKey['longitude']);
+        altitude = Number(outerKey['altitude']);
+        status = Number(outerKey['status']);
         name = outerKey['name'];
         token = outerKey['token'];
         
@@ -142,7 +144,8 @@ router.post('/facebookpost', function(request, response){
                                                             // returns the post id
                                                             console.log(res); // { id: xxxxxx
                                                             console.log("XDDD");
-                                                            facebookPagePost(token);
+                                                            //facebookPagePost(token);
+                                                            facebookPagePost(token,name,status,data.results[0].formatted_address);
                                                             response.send({['fb_id'] : fb_id, ['message'] : "Hang on " + name + ". First responders are on their way."});
                                                         }
                                                     });
@@ -173,20 +176,21 @@ router.post('/facebookpost', function(request, response){
 
 router.get('/facebookget', function(request, response){
     var responseArray = [];
-    
+    var str = '';
     db.ref('/users').once('value').then(function(snapshot){
         var val = snapshot.val()
         for (var key in val) {
             if (val.hasOwnProperty(key)) {
-                responseArray.push({fb_id : val[key]['fb_id'], name : val[key]['name'], latitude : val[key]['latitude'], 
-                    longitude : val[key]['longitude'], altitude : val[key]['altitude']});
+                str += val[key]['latitude'] + ',' + val[key]['longitude'] + ',';
+                // responseArray.push(val[key]['latitude']);
+                // responseArray.push(val[key]['longitude']);
             }
         }
-        response.send(responseArray);
+        response.send(str);
     });
 });
 
-var facebookPagePost = function(token){
+var facebookPagePost = function(token,name,status,address){
     graph.setVersion('2.11');
     
     var user_token = token;
@@ -212,25 +216,30 @@ var facebookPagePost = function(token){
                             var tag_string = "";
                             console.log(last_name);
                             for (var g = 0; g < resp.data.length; g++) {
-                                //console.log(resp.data[g].name)
                                 if (contact_names[resp.data[g].name] != null) {
-                                    //console.log("FOUND ONE LOL")
                                     if (tag_string != "") tag_string = tag_string + ", ";
                                     tag_string = tag_string + resp.data[g].id;
-                                } else if (resp.data[g].name.split(" ")[1] == last_name) {
+                                }/* else if (resp.data[g].name.split(" ")[1] == last_name) {
                                     if (tag_string != "") tag_string = tag_string + ", ";
                                     tag_string = tag_string + resp.data[g].id;
-                                }
+                                }*/
                             }
                             console.log("Tag String", tag_string);
-                            var cfg = { "page_id" : "522354384791944", "access_token" : "EAACrIZCHE7c8BAPTPdStymmLSwtgHVpT7TspAZAk9Da0tuk7ZCAfSVt6Vp9vtaXcAqmkrnwyZCwP7jws3f6bUhw3AS0Ok1jkC0qeOfi5siCZCfOsyVRc4nMHZA4VNmnsJ7zWW9Wjomc56LUQujZBfN1jmlen00WcQ4h64LNCOxCyQZDZD"};
-                            graph.setAccessToken(cfg['access_token']);
+                            //var cfg = { "page_id" : "522354384791944", "access_token" : "EAACrIZCHE7c8BAPTPdStymmLSwtgHVpT7TspAZAk9Da0tuk7ZCAfSVt6Vp9vtaXcAqmkrnwyZCwP7jws3f6bUhw3AS0Ok1jkC0qeOfi5siCZCfOsyVRc4nMHZA4VNmnsJ7zWW9Wjomc56LUQujZBfN1jmlen00WcQ4h64LNCOxCyQZDZD"};
+                            //var cfg = { "page_id" : "522354384791944", "access_token" : "EAACrIZCHE7c8BAOZC8tIPvyL3ZCFckvyWrjshwv9XpmnzTeSICFDPBcnr1mrsZBaV27sPfNszJDjtn8LZAU7XFQ9P0djlM8eHqZB6eZABwmKSZBkvATsZBntVq2OfSqg3HJhsbfjcZAZAGsASFT02WeJZAzIm76d2tBP0hD2fFIZCaM0FAAF2INIFcxCM"};
+        
+                            var cfg = { "page_id" : "126217038058537", "access_token" : "EAACrIZCHE7c8BANwJn5AzhbUxV80ZCGxRpxniK7ZC1lE5APSnXqQpfM8IbHfuyKS0qBiS9F34S83t8utegXdShNXVZBlZAlWM89TajM9HdNAgolzjMYnw4LbvKmN7V8xwGr7JhMCUiHIsED92oTmE4A52sfH4UCzcP1aJYyPM5jAU9jiSrZCR2"};
+                            
+                            graph.setAccessToken(user_token);
+                            var state = "";
+                            if (status == 0) state = "Needs Assistance";
+                            else state = "Safe";
                             var wallPost = {
-                              message: "Hello World10",
-                              tags: tag_string
+                                message: name + "\n" + state + "\n" + address,
+                                tags: tag_string
                             };
                              
-                            graph.post(cfg['page_id'] + "/feed", wallPost, function(err, res) {
+                            graph.post("126217038058537/feed", wallPost, function(err, res) {
                               // returns the post id 
                               if(err)console.log(err);
                               else console.log(res);
